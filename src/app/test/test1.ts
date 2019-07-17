33f85452341713e340dd8c74dea4e324e092b535
olgagorobchenko <olga.gorobchenko@egt-ua.com>
2019-07-17 15:55:24 +0300

    test

--- src/app/test/test1.ts
+++ src/app/test/test1.ts
@@ -2,4 +2,7 @@ import { Type } from '@angular/core';
 
 export const arr5: Array<Array<Type<string>>> = [];
 export const rrr: Array<Type<string>> = [];
-console.log(arr5, rrr);
+export const www: Array<Type<string> | Type<number>> = [];
+export const ttt: Map<string, Array<string>> = new Map([['test', []]]);
+
+console.log(arr5, rrr, www, ttt);
