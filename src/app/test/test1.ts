e25744cf1de403a6d0d5a78ebdc7378f9085bccc
olgagorobchenko <olga.gorobchenko@egt-ua.com>
2019-07-17 13:37:14 +0300

    test

--- src/app/test/test1.ts
+++ src/app/test/test1.ts
@@ -1,16 +1,4 @@
 import { Type } from '@angular/core';
 
-// export const arr1: Array<string> = [];
-// console.log(arr1);
-//
-// export const arr2: Array<string | number | boolean> = [];
-// console.log(arr2);
-//
-// export const arr3: Array<string> = [];
-// console.log(arr3);
-//
-// export const arr4: Array<Type<string>> = [];
-// console.log(arr4);
-
 export const arr5: Array<Array<Type<string>>> = [];
 console.log(arr5);
