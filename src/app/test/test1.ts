863e6b7e9e1f9d3aed86f39b1a974b07c1f159ec
olgagorobchenko <olga.gorobchenko@egt-ua.com>
2019-07-17 19:21:50 +0300

    test

--- src/app/test/test1.ts
+++ src/app/test/test1.ts
@@ -7,6 +7,13 @@ export const ttt: Map<string, Array<string>> = new Map([['test', []]]);
 
 console.log(arr5, rrr, www, ttt);
 
+export interface ISocialSettings {
+    description?: string;
+    image?: string;
+    title?: string;
+    type?: string;
+}
+
 export function groupByDay<T>(data: Array<T>, timestampField: string): Map<string, Array<T>> {
     const resultMap: Map<string, Array<T>> = new Map();
     for (const item of data) {
@@ -31,10 +38,5 @@ const suitableLocales = [].reduce<any>((acc: Array<Array<string>>, locale: strin
 }, []);
 console.log('suitableLocales', suitableLocales);
 
-const social_settings: Array<{
-    description?: string,
-    image?: string,
-    title?: string,
-    type?: string,
-}> = [{}];
+const social_settings: Array<ISocialSettings> = [{}];
 console.log(social_settings);
