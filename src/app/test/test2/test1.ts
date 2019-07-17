5fabdf39791c31297e33af3b0caad7ad9c27583a
olgagorobchenko <olga.gorobchenko@egt-ua.com>
2019-07-17 17:54:59 +0300

    test

---
+++ src/app/test/test2/test1.ts
@@ -0,0 +1,32 @@
+import { Type } from '@angular/core';
+
+export const arr5: Array<Array<Type<string>>> = [];
+export const rrr: Array<Type<string>> = [];
+export const www: Array<Type<string> | Type<number>> = [];
+export const ttt: Map<string, Array<string>> = new Map([['test', []]]);
+
+console.log(arr5, rrr, www, ttt);
+
+export function groupByDay<T>(data: Array<T>, timestampField: string): Map<string, Array<T>> {
+    const resultMap: Map<string, Array<T>> = new Map();
+    for (const item of data) {
+        const dateString = timestampField;
+        const dateGroup = resultMap.get(dateString);
+        if (dateGroup) {
+            dateGroup.push(item);
+        } else {
+            resultMap.set(dateString, [item]);
+        }
+    }
+    return resultMap;
+}
+
+// Flags for lang switcher
+const suitableLocales = [].reduce<any>((acc: Array<Array<string>>, locale: string) => {
+    const countryCode = locale.split('_')[1] || '';
+    if (countryCode.length === 2) {
+        acc.push([locale, `flags/${countryCode}.svg`]);
+    }
+    return acc;
+}, []);
+console.log('suitableLocales', suitableLocales);
